abbcfb70a05b3dbd2e8b37f8dae3af3069c548da
Amith <amithkumarnarayan2@gmail.com>
2019-08-22 02:20:35 +0100

    Initial Commit

---
+++ coreDataToDo/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  coreDataToDo
+//
+//  Created by Amith Kumar Narayan on 17/08/2019.
+//  Copyright © 2019 Amith. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController : UITableViewController {
+
+    let itemArray = ["Amith", "Ranjan", "Mark", "Tony"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //this is a test for version controle.
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellID", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+}
+
