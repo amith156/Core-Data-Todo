dae4de88f0c5cf7dcf194c306ef300ecfdae8913
Amith <amithkumarnarayan2@gmail.com>
2019-08-30 05:22:38 +0100

    adds check mark when tapped and removes checkmark when double taped.

--- coreDataToDo/TodoListViewController.swift
+++ coreDataToDo/TodoListViewController.swift
@@ -20,6 +20,8 @@ class TodoListViewController : UITableViewController {
 
     }
 
+    //MARK - TableView Datasourse methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -29,6 +31,20 @@ class TodoListViewController : UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellID", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }
+    
+    //MARK - TableView Delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
 
 }
